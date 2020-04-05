cdf4cb5119aaa97eee9e2e9af3d452ef9e067983
Phongtlam <Phongtlam@gmail.com>
2020-04-05 02:43:14 -0700

    Updates

---
+++ precache-manifest.60b0963546d1adfb4a3f35ef8207cd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95529c1b825e5b59c03bcce60cfaa3e",
+    "url": "/react-checkers/index.html"
+  },
+  {
+    "revision": "6abd8417a544de963f7b",
+    "url": "/react-checkers/static/css/main.a3ba8477.chunk.css"
+  },
+  {
+    "revision": "020a04f5d5c6d7b37b3d",
+    "url": "/react-checkers/static/js/2.bdb5b5bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-checkers/static/js/2.bdb5b5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6abd8417a544de963f7b",
+    "url": "/react-checkers/static/js/main.4ceeb13e.chunk.js"
+  },
+  {
+    "revision": "869ece9498aca0648938",
+    "url": "/react-checkers/static/js/runtime-main.54b3b4ba.js"
+  }
+]);
